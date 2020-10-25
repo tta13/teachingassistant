707b70858c8b2e7b576cfd99845280457eb22fe2
tta13 <tta@cin.ufpe.br>
2020-10-24 23:32:19 -0300

    Adicionando testes para a funcionalidade de remover alunos

--- tests-acceptance/stepdefinitions/alunos.ts
+++ tests-acceptance/stepdefinitions/alunos.ts
@@ -1,10 +1,12 @@
 import { defineSupportCode } from 'cucumber';
-import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+import { browser, $, element, ElementArrayFinder, by, ElementFinder } from 'protractor';
+import { Alert } from 'selenium-webdriver';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
 let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
 let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
+let clickOnRemove = ((elem) => elem.element(by.name('removelist')).click());
 
 let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
 
@@ -15,6 +17,7 @@ defineSupportCode(function ({ Given, When, Then }) {
         await $("a[name='alunos']").click();
     })
 
+    //Scenario: Registering student with non registered CPF
     Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
         var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
         var samecpfs = allcpfs.filter(elem =>
@@ -34,4 +37,25 @@ defineSupportCode(function ({ Given, When, Then }) {
                    (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
     });
 
+    //Scenario: Removing student from students list
+    Given(/^I can see a student "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
+        await $("input[name='namebox']").sendKeys(<string> name);
+        await $("input[name='cpfbox']").sendKeys(<string> cpf);
+        await element(by.buttonText('Adicionar')).click();
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await allalunos.filter(elem => pAND(sameCPF(elem,cpf),sameName(elem,name))).then
+                    (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
+
+    When(/^I try to remove the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await clickOnRemove(allalunos.filter(elem => pAND(sameCPF(elem,cpf), sameName(elem,name))).first());
+        await browser.switchTo().alert().accept();
+    });
+
+    Then(/^I can no longer see the student "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {        
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await allalunos.filter(elem => pAND(sameCPF(elem,cpf),sameName(elem,name))).then
+                   (elems => expect(Promise.resolve(elems   .length)).to.eventually.equal(0));
+    });
 })
